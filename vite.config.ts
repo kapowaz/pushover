a9d5946d1e6bfc2d396442a04ee2198c5b2bf9f5
Ben Darlow <github@kapowaz.net>
2026-03-08 16:01:35 +0000

    Add MIT license and deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES ? '/pushover/' : '/',
   plugins: [vanillaExtractPlugin()],
 });
